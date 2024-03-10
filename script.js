8bf27547a5f466648c5b851c573778897386ffef
AndresASG <94728345+AndresASG@users.noreply.github.com>
2024-03-10 16:14:57 -0400

    error 405 arreglado

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const boton1 =  document.querySelector("#boton1");
-const boton2 =  document.querySelector("#boton2");
-const modal =  document.querySelector("#modal");
 
-boton1.addEventListener( "click",()=>{
-    modal.showmodal();
-});
