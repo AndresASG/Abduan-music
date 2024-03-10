d91e86b5730c26fc6dfd9489dc31bbca1f42ebfc
AndresASG <94728345+AndresASG@users.noreply.github.com>
2024-03-09 21:14:14 -0400

    prueba

---
+++ script.js
@@ -0,0 +1,7 @@
+const boton1 =  document.querySelector("#boton1");
+const boton2 =  document.querySelector("#boton2");
+const modal =  document.querySelector("#modal");
+
+boton1.addEventListener( "click",()=>{
+    modal.showmodal();
+});
